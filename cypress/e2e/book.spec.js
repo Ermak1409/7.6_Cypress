cf2baac6d6bdf81cac55c24a30572cc0c2f48bb1
Ermak <ermak1409@gmail.com>
2022-09-19 21:32:56 +0300

    new

---
+++ cypress/e2e/book.spec.js
@@ -0,0 +1,40 @@
+describe("When user add the books", () => {
+
+  beforeEach(() => {
+      cy.visit(' ');
+      cy.login("test@test.com", "test");
+  });
+
+  it("Should be able to add a book", () => {
+      cy.typeForm(
+          "Test book",
+          "Vasilii",
+          "Vasilii"
+      );
+      cy.contains("Submit").click();
+      cy.contains("Test book");
+  });
+
+  it("Should be able to add a book to favorite", () => {
+      cy.get(".card-body").contains("Test book").parent().siblings().children("button").click();
+      cy.contains("Favorites").click();
+      cy.contains("Test book");
+  });
+  
+  it("Should be able to delete a book from favorite", () => {
+      cy.contains("Favorites").click();
+      cy.get(".card-body").contains("Test book").parent().siblings().children("button").click();
+      cy.contains("Please add some book to favorit on home page!");
+  });
+
+  it("Should be close add book form", () => {
+      cy.typeForm(
+          "Qwer", 
+          " ", 
+          " "
+      );
+      cy.contains("Cancel").click();
+      cy.contains("Books list");
+  });
+ 
+})
